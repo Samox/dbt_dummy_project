3aa55bebcda986ee7b44bbbdf245f07dd379fa11
Sammy Teillet <sammy.teillet@gmail.com>
2023-09-27 17:10:26 +0200

    feature: add push_metric notebook

--- models/metrics/monthly_bookings.sql
+++ models/metrics/monthly_bookings.sql
@@ -1,3 +1,12 @@
+{{
+    config(
+        meta={
+            "datadrift":true,
+            "history_retention_period":"1 year",
+        }
+    )
+}}
+
 SELECT 
     date_trunc('month', to_date(date, 'YYYY-MM-DD')) AS month,
     SUM(metric_value) AS monthly_metric
