8ad7712bb824cbcca5e81a1f267b4ac1354c94eb
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-17 09:29:09 +0200

    feature: round the result in monthly bookings

--- models/metrics/monthly_bookings.sql
+++ models/metrics/monthly_bookings.sql
@@ -10,7 +10,7 @@
 
 SELECT 
     date_trunc('month', booking_date)::DATE AS month,
-    SUM(metric_value) AS monthly_booking_value
+    CAST(SUM(metric_value) AS NUMERIC(10, 2)) AS monthly_booking_value
 FROM {{ref('bookings')}}
 GROUP BY month
 ORDER BY month
