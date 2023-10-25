1a27111a202446ce14cb48179405eb2a97da541e
Sammy Teillet <sammy.teillet@gmail.com>
2023-10-25 12:01:15 +0200

    feature: add created and updated dates

--- models/mart/bookings.sql
+++ models/mart/bookings.sql
@@ -12,5 +12,7 @@ SELECT
   unique_key,
   to_date(date, 'YYYY-MM-DD')::DATE AS booking_date,
   metric_value,
-  country_code
+  country_code,
+  created_at,
+  updated_at
 FROM {{ source('bookings', 'source_bookings') }}
