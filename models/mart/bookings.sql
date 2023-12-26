c4b9bf2b6eb61e3efe1cf88a64321b64c3afa93b
Sammy Teillet <sammy.teillet@gmail.com>
2023-12-26 12:16:50 +0400

    add metrics

--- models/mart/bookings.sql
+++ models/mart/bookings.sql
@@ -1,15 +1,5 @@
-{{
-    config(
-        meta={
-            "datadrift":true,
-            "datadrift_unique_key":"unique_key",
-            "datadrift_date":"booking_date"
-        }
-    )
-}}
-
 SELECT
-  unique_key,
+  unique_key as booking_id,
   to_date(date, 'YYYY-MM-DD')::DATE AS booking_date,
   metric_value,
   country_code,
